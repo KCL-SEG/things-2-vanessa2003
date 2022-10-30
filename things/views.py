01b8a0eac23f3c96eb1ee50c5a86fb3167903a92
Vanessa Neboh <vanessa.neboh@kcl.ac.uk>
2022-10-30 21:16:20 +0000

    add things url path

--- things/views.py
+++ things/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .forms import ThingForm
 
+
 def home(request):
+    return render(request, 'home.html')
+
+def things(request):
     form = ThingForm()
     return render(request, 'thing_sign_up.html' , {'form': form})
